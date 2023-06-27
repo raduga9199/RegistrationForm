09814b07b08da45182ae1496130feca05673b0d0
raduga9199 <raduga9199@gmail.com>
2023-06-27 10:30:08 -0400

    Employee List

--- src/main/java/com/cydeo/employeeregistrationproject/bootstrap/DataGenerator.java
+++ src/main/java/com/cydeo/employeeregistrationproject/bootstrap/DataGenerator.java
@@ -1,9 +1,23 @@
 package com.cydeo.employeeregistrationproject.bootstrap;
 
+import com.cydeo.employeeregistrationproject.model.Employee;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DataGenerator {
+
+    private static List<Employee>employeeList = new ArrayList<>();
+
+    public static void saveEmployee(Employee employee){
+        employeeList.add(employee);
+    }
+
+    public static List<Employee> readAllEmployees(){
+        return employeeList;
+    }
+
     public static List<String> getAllStates(){
         return Arrays.asList(
                 "Alabama",
--- src/main/java/com/cydeo/employeeregistrationproject/controller/EmployeeController.java
+++ src/main/java/com/cydeo/employeeregistrationproject/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import com.cydeo.employeeregistrationproject.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,6 +18,15 @@ public class EmployeeController {
         model.addAttribute("employee", new Employee());
         model.addAttribute("states", DataGenerator.getAllStates());
         return "employee/employee-create";
+    }
+
+    @PostMapping("/list")
+    public String employeeList(@ModelAttribute("employee") Employee employee,Model model){
+        DataGenerator.saveEmployee(employee);
+
+        model.addAttribute("employees",DataGenerator.readAllEmployees());
+
+        return "employee/employee-list";
     }
 
 
