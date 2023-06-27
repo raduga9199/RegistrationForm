5c24bdf13e4bc1ebc8197651e2a036464a505d51
raduga9199 <raduga9199@gmail.com>
2023-06-27 10:09:12 -0400

    states

--- src/main/java/com/cydeo/employeeregistrationproject/controller/EmployeeController.java
+++ src/main/java/com/cydeo/employeeregistrationproject/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.cydeo.employeeregistrationproject.controller;
 
 
+import com.cydeo.employeeregistrationproject.bootstrap.DataGenerator;
+import com.cydeo.employeeregistrationproject.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,9 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/employee")
 public class EmployeeController {
     @GetMapping("/register")
-    public String createEmployee(){
+    public String createEmployee(Model model){
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("states", DataGenerator.getAllStates());
         return "employee/employee-create";
     }
+
+
 
 
 }
